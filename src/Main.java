cd538212a5eefa6b044480cd27c480df076665ca
Максим <gmax7911@gmail.com>
2022-12-21 13:38:35 +0300

    Задача 1 Выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 public class Main {
+
+    // Задача 1.
     public static void main(String[] args) {
+        printYear(2020);
+        printYear(3061);
     }
+
+    public static boolean checkingTheYear(int year) {
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+    public static void printYear(int year) {
+        if (checkingTheYear(year)) {
+            System.out.println(year + " Год является високосным");
+        } else {
+            System.out.println(year + " Год не является високосным");
+        }
+    }
+
 }
